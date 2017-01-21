e22dbeaebf0e1ab84928009ce4c4c3e5d7d93095
Xinhai Xu <xxh2358@gmail.com>
2017-01-21 02:37:57 -0500

    a lovely character can walk

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed = 1f;
+    Animator anim;
+    private int rot = 0;
+
+	// Use this for initialization
+	void Start ()
+    {
+        anim = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector2 dir = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.rotation = Quaternion.AngleAxis(0, Vector3.forward);
+            dir = Vector2.up;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.rotation = Quaternion.AngleAxis(180, Vector3.forward);
+            dir = Vector2.up;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.rotation = Quaternion.AngleAxis(90, Vector3.forward);
+            dir = Vector2.up;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.rotation = Quaternion.AngleAxis(270, Vector3.forward);
+            dir = Vector2.up;
+        }
+        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.A))
+        {
+            transform.rotation = Quaternion.AngleAxis(45, Vector3.forward);
+            dir = Vector2.up;
+        }
+        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D))
+        {
+            transform.rotation = Quaternion.AngleAxis(315, Vector3.forward);
+            dir.x = 1;
+            dir.y = 1;
+            dir = Vector2.up;
+        }
+        if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A))
+        {
+            transform.rotation = Quaternion.AngleAxis(135, Vector3.forward);
+            dir.x = -1;
+            dir.y = -1;
+            dir = Vector2.up;
+        }
+        if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D))
+        {
+            transform.rotation = Quaternion.AngleAxis(225, Vector3.forward);
+            dir.x = 1;
+            dir.y = -1;
+            dir = Vector2.up;
+        }
+
+        //not walking
+        if (dir.Equals(Vector2.zero))
+            anim.SetBool("walking", false);
+        else
+        {
+            anim.SetBool("walking", true);
+        }
+
+        transform.Translate(dir * speed * Time.deltaTime);
+	}
+}
