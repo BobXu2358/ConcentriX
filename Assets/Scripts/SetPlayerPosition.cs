585e0282786281101e62c0adefe055bd4be7b4c7
André Santiago <Andr Santiago>
2017-01-21 17:23:31 -0500

    Added basis for first maze

---
+++ Assets/Scripts/SetPlayerPosition.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetPlayerPosition : MonoBehaviour {
+
+    public Transform player;
+
+	// Use this for initialization
+	void Start () {
+        player.transform.position = this.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
