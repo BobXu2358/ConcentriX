6b93c5f52cfe66e3997c0d446bbdb589d49f238a
Xinhai Xu <xxh2358@gmail.com>
2017-01-22 12:39:11 -0500

    scene transition, UI, game manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public bool chip1 = false;
+    public bool chip2 = false;
+    public bool chip3 = false;
+    public int hp = 1;
+    public int maxEnergy = 3;
+    public int chances = 3;
+
+	// Use this for initialization
+	void Start () {
+        DontDestroyOnLoad(this);
+        Application.LoadLevel("menu");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(chip1 && chip2 && chip3)
+        {
+            Application.LoadLevel("accomplish");
+        }
+        if(hp <= 0)
+        {
+            chances--;
+            Application.LoadLevel("fail");
+        }
+        if(chances <= 0)
+        {
+            Application.LoadLevel("dead");
+        }
+	}
+}
